f3ea1af8894151d62b90379f3897d27f3b908680
Aneta Halz <aneta.halz@interia.pl>
2017-02-25 13:23:23 +0100

    Kodilla zad.8.4

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ console.log('Witaj, ' + name);
 /*komentarz blokowy
 c.d. komentarza blokowego
 */
+
+// pole trójkąta
+var a = 5,
+	h = 3,
+	triangleArea = a * h / 2;
+
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
