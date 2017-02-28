e26f8020dd4337635e7cfc000fbb343958ae7cda
Aneta Halz <aneta.halz@interia.pl>
2017-02-28 11:31:40 +0100

    correction

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
-// plik scripts.js
-
 var name = prompt('Wpisz swoje imię');
 // alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
-/*komentarz blokowy
+
+/*
+komentarz blokowy
 c.d. komentarza blokowego
 */
 
-// pole trójkąta
+// pole trójkąta zad. 8.4
 var a = 5,
 	h = 3,
 	triangleArea = a * h / 2;
-
 
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
